449da3011a54ab98475323c61eb6ba8c196e1953
Aritz Beraza <aritz.beraza@gmail.com>
2018-11-01 19:46:12 +0100

    Code cleanup

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -41,80 +41,33 @@ class DBHelper {
     getJSON(DBHelper.DATABASE_URL)
       .then(restaurants => callback(null, restaurants))
       .catch(error => callback(error, null));
-    // xhr.open('GET', DBHelper.DATABASE_URL);
-    // xhr.onload = () => {
-    //   if (xhr.status === 200) { // Got a success response from server!
-    //     const json = JSON.parse(xhr.responseText);
-    //     const restaurants = json.restaurants;
-    //     callback(null, restaurants);
-    //   } else { // Oops!. Got an error from server.
-    //     const error = (`Request failed. Returned status of ${xhr.status}`);
-    //     callback(error, null);
-    //   }
-    // };
-    // xhr.send();
   }
 
   /**
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    // fetch(RESTAURANT_BY_ID(id))
     getJSON(RESTAURANT_BY_ID(id))
       .then(restaurant => callback(null, restaurant))
       .catch(error => callback(error, null));
-
-    // DBHelper.fetchRestaurants((error, restaurants) => {
-    //   if (error) {
-    //     callback(error, null);
-    //   } else {
-    //     const restaurant = restaurants.find(r => r.id == id);
-    //     if (restaurant) { // Got the restaurant
-    //       callback(null, restaurant);
-    //     } else { // Restaurant does not exist in the database
-    //       callback('Restaurant does not exist', null);
-    //     }
-    //   }
-    // });
   }
 
   /**
    * Fetch restaurants by a cuisine type with proper error handling.
    */
   static fetchRestaurantByCuisine(cuisine, callback) {
-    // Fetch all restaurants  with proper error handling
     getJSON(RESTAURANT_BY_CUISINE(cuisine))
       .then(restaurants => callback(null, restaurants))
       .catch(error => callback(error, null));
-    // DBHelper.fetchRestaurants((error, restaurants) => {
-    //   if (error) {
-    //     callback(error, null);
-    //   } else {
-    //     // Filter restaurants to have only given cuisine type
-    //     const results = restaurants.filter(r => r.cuisine_type == cuisine);
-    //     callback(null, results);
-    //   }
-    // });
   }
 
   /**
    * Fetch restaurants by a neighborhood with proper error handling.
    */
   static fetchRestaurantByNeighborhood(neighborhood, callback) {
-    // Fetch all restaurants
     getJSON(RESTAURANT_BY_NEIGHBORHOOD(neighborhood))
       .then(restaurants => callback(null, restaurants))
       .catch(error => callback(error, null));
-    // DBHelper.fetchRestaurants((error, restaurants) => {
-    //   if (error) {
-    //     callback(error, null);
-    //   } else {
-    //     // Filter restaurants to have only given neighborhood
-    //     const results = restaurants.filter(r => r.neighborhood == neighborhood);
-    //     callback(null, results);
-    //   }
-    // });
   }
 
   /**
@@ -131,29 +84,12 @@ class DBHelper {
     getJSON(RESTAURANT_BY_FILTER(filter))
       .then(restaurants => callback(null, restaurants))
       .catch(error => callback(error, null));
-    // Fetch all restaurants
-    // DBHelper.fetchRestaurants((error, restaurants) => {
-    // Restaurants((error, restaurants) => {
-    //   if (error) {
-    //     callback(error, null);
-    //   } else {
-    //     let results = restaurants
-    //     if (cuisine != 'all') { // filter by cuisine
-    //       results = results.filter(r => r.cuisine_type == cuisine);
-    //     }
-    //     if (neighborhood != 'all') { // filter by neighborhood
-    //       results = results.filter(r => r.neighborhood == neighborhood);
-    //     }
-    //     callback(null, results);
-    //   }
-    // });
   }
 
   /**
    * Fetch all neighborhoods with proper error handling.
    */
   static fetchNeighborhoods(callback) {
-    // Fetch all restaurants
     DBHelper.fetchRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
